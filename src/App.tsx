51cc9302f4d8c9d0e6637a330f9d5258f6b67ed8
Jonatan Petersson <petersson.jonatan@gmail.com>
2023-03-19 13:04:28 +0100

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import namesWithBackgroundLogo from "./assets/images/names_and_watercolour.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <img src={namesWithBackgroundLogo} alt="Elias & Nellie Logo" />
+    </>
+  );
 }
 
 export default App;
