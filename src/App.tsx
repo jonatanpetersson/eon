ea762f67a1f71630e724c3e21c6fce40ebc322bc
Jonatan Petersson <petersson.jonatan@gmail.com>
2023-04-20 22:11:14 +0200

    Added party info

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
     <>
-      <div className="welcome">
+      <div className="wedding">
         <p>
           Varmt välkommen att
           <br />
@@ -23,8 +20,20 @@ function App() {
           566 92 Habo
         </p>
       </div>
+      <div className="party">
+        <p>
+          Efter vigseln blir det middag och fortsatta festligheter på Spinnet i
+          Habo
+        </p>
+        <p>
+          På festen önskar vi fira med våra vuxna vänner (undantag ammande barn)
+        </p>
+        <p>Klädsel: Det du känner dig fin i</p>
+        <p>
+          Din närvaro är det viktigaste för oss, men om du vill ge en gåva finns
+          vår önskelista <a href="https://presentlistan.nu/C5B4F2">här</a>
+        </p>
+      </div>
     </>
   );
-}
-
-export default App;
+};
